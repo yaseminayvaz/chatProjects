3c01efa81af5bef66ed9bbdaf819955c13d951e5
Yasemin Ayvaz <yaseminayvaz@protonmail.com>
2023-09-14 14:21:11 +0300

    console emütör cihazdaki mesaj içeriğini yazdırma

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {useEffect, useState} from 'react';
+import * as Permissions from 'expo-permissions';
+import SmsRetriever from 'react-native-sms-retriever';
 
-export default function App() {
+
+export default function App() { 
+ /* useEffect(() => {
+    async function requestPermissions() {
+      const { status } = await Permissions.askAsync(Permissions.SMS);
+      if (status === 'granted') {
+        console.log('SMS izni verildi.');
+
+        SmsRetriever.startSmsRetriever();
+
+        SmsRetriever.addSmsListener(event => {
+          if (event.message) {
+            console.log('Gelen SMS:', event.message);
+          }
+        });
+      } else {
+        console.log('SMS izni reddedildi.');
+      }
+    }
+    requestPermissions();
+  }, []);
+*/
+useEffect(() => {
+  async function startSmsRetriever() {
+    try {
+      const registered = await SmsRetriever.startSmsRetriever();
+      if (registered) {
+        SmsRetriever.addSmsListener(event => {
+          if (event.message) {
+            console.log('Gelen SMS:', event.message);
+          }
+        });
+      } else {
+        console.error('SMS dinleme başlatılamadı.');
+      }
+    } catch (error) {
+      console.error('SMS dinleme hatası:', error);
+    }
+  }
+  startSmsRetriever();
+}, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <Text>Hello, Expo SMS Reader</Text>
     </View>
   );
 }
